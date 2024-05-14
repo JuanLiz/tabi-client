28e5810b339903b94ad9082981483d723d546541
JuanLiz <juand5455@gmail.com>
2024-05-13 23:25:28 -0500

    fix (config): Fixed font incompatibility for mobile browsers

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
             theme={{
               token: {
                 // Seed Token
-                fontFamily: "Mulish",
+                fontFamily: "inherit",
 
                 borderRadius: 8,
 
